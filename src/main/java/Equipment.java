fd60f907d206916daaf530c04885a8de39234f38
Tom Mutimer <tmutimer96@gmail.com>
2020-05-20 14:04:57 +0100

    Expand the Equipment class

--- src/main/java/Equipment.java
+++ src/main/java/Equipment.java
@@ -3,8 +3,16 @@
  */
 public class Equipment {
     private int[] mId;
+
+    // Example name: 'PCR (047)'.
     private String mName;
+
+    // Not currently necessary for solving, but may be useful if approach is changed.
+    private Room mRoom;
+
+    // Number of pieces of equipment of the type (and Room associated).
     private int mNumberOfEquipment;
+
 
     public int[] getId() {
         return mId;
